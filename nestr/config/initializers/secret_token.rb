bd4ab6ccd638813922e3ab6668b4b39d84174aeb
juliekraft <julieschmoo@gmail.com>
2014-01-13 11:04:18 -0500

    made new rails app to practice pushing to development

---
+++ nestr/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Nestr::Application.config.secret_token = '4a8539fc44af67782db6f27c41f95eb973a6e34c893ed6952cd346c0ea6731af80b0d4e2dee00cfad03514eff0116e46c188a8987b21cea19adcad4ca764c8e8'
