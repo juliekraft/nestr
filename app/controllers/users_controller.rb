8193b88d0286b607c768b8355c223cb46fc20322
juliekraft <julieschmoo@gmail.com>
2014-01-15 13:52:15 -0500

    added elsif to users controller index

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,9 +17,11 @@ class UsersController < ApplicationController
     #THIS IF STATEMENT ISN'T WORKING -- it just goes to root path
     # @is_broker = current_user.find(params[:is_a_broker]) #???
     if current_user.is_a_broker
-     render :"broker/index"
+      render :"broker/index"
+    elsif current_user == nil
+      redirect_to new_user_registration_path
     else 
-     render :"renter/index"
+      render :"renter/index"
     end
   end
 
